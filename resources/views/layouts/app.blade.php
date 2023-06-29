f8eaf7743fa6fadf93f36282ee7981b812047e2c
vanstrouble <vanstrouble@proton.me>
2023-06-29 16:22:21 -0600

    Add href to /create-account

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,21 @@
                 </h1>
                 <nav class=" flex gap-2 items-center">
                     <a class=" font-bold uppercase text-gray-600 text-sm" href="">Login</a>
-                    <a class=" font-bold uppercase text-gray-600 text-sm" href="">Crear Cuenta</a>
+                    <a class=" font-bold uppercase text-gray-600 text-sm" href="/create-account">Crear Cuenta</a>
                 </nav>
             </div>
         </header>
+
+        <main class=" container mx-auto mt-10">
+            <h2 class=" font-black text-center text-3xl mb-10">
+                @yield('title')
+            </h2>
+            @yield('content')
+        </main>
+
+        <footer class=" text-center p-5 text-gray-500 font-bold uppercase text-sm">
+            Devstagram - Todos los derechos reservados {{ now()->year }}
+        </footer>
+
     </body>
 </html>
