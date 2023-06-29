62a031d8ab398ed0acdc8e56f3803f7c69cac799
vanstrouble <vanstrouble@proton.me>
2023-06-29 16:57:40 -0600

    Modify route with RegisterController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('principal');
 });
 
-Route::get('/create-account', function () {
-    return view('auth.register');
-});
+Route::get('/create-account', [RegisterController::class,'index']);
