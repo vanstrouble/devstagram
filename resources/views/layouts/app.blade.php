165ad8be37a701ce0f0dcddf53a58ad0f3452214
vanstrouble <vanstrouble@proton.me>
2023-06-29 15:16:20 -0600

    Create Devstagram header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,17 @@
 
     </head>
 
-    <body>
-        <h1 class="text-4xl">
-            @yield('title')
-        </h1>
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class=" container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">
+                    Devstagram
+                </h1>
+                <nav class=" flex gap-2 items-center">
+                    <a class=" font-bold uppercase text-gray-600 text-sm" href="">Login</a>
+                    <a class=" font-bold uppercase text-gray-600 text-sm" href="">Crear Cuenta</a>
+                </nav>
+            </div>
+        </header>
     </body>
-
 </html>
