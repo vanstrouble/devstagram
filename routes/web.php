c8556b9957c369d88738d52336d06b2bd123de3b
vanstrouble <vanstrouble@proton.me>
2023-06-29 16:23:38 -0600

    Define get route to url /create-account

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('principal');
 });
+
+Route::get('/create-account', function () {
+    return view('auth.register');
+});
