b0b4e3e35b89d52aa2aeadc44169492a51304f9d
vanstrouble <vanstrouble@proton.me>
2023-06-30 12:30:35 -0600

    Add validations

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,6 +11,13 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request) {
-        dd($request);
+        // dd($request);
+        $this->validate($request, [
+            'name' => ['required', 'min:4', 'max:30', 'regex:/^[A-Za-záéíóúÁÉÍÓÚ\s]+$/u'],
+            'username' => ['required', 'alpha_dash','unique:users', 'min:4', 'max:20'],
+            'email' => ['required', 'email','unique:users', 'max:50'],
+            'password' => ['required', 'confirmed',],
+        ]);
+
     }
 }
