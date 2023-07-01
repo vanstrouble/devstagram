e1d01d3188c88854eb66e9a9881f260da667e442
vanstrouble <vanstrouble@proton.me>
2023-07-01 16:50:10 -0600

    Create dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,5 +5,19 @@
 @endsection
 
 @section('content')
-    Here are your ugly baby photos
+    <div class=" flex justify-center">
+        <div class=" w-full md:w-8/12 lg:w-6/12 md:flex">
+            <div class=" md:w-8/12 lg:w-6/12 px-5">
+                <p>Ur horrible face here</p>
+                <img src="{{ asset('img/usuario.svg') }}" alt="user image">
+            </div>
+            <div class=" md:w-8/12 lg:w-6/12 px-5">
+                @if(auth()->check())
+                    <p class=" text-gray-700 text-2xl">{{ auth()->user()->username }}</p>
+                @else
+                    <p class=" text-gray-700 text-2xl">No user authenticated</p>
+                @endif
+            </div>
+        </div>
+    </div>
 @endsection
