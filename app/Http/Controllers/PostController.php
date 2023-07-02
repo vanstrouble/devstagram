420c12139aad4b96f94bef315fac623ff50278a2
vanstrouble <vanstrouble@proton.me>
2023-07-02 11:41:03 -0600

    Protect dashboard view user

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     //
-    public function index() {
+    public function index()
+    {
         return view('dashboard');
     }
 }
