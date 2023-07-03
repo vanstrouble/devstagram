c5f345b5c30baa2c36e16bc1c0734dfbba476c5b
vanstrouble <vanstrouble@proton.me>
2023-07-03 16:57:23 -0600

    Keep session opened functionality

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,12 +14,14 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
+        // dd($request->remember); // Testing Remember checkbox
+
         $this->validate($request, [
             'email' => ['required', 'email',],
             'password' => ['required',],
         ]);
 
-        if(!auth()->attempt($request->only('email', 'password')))
+        if(!auth()->attempt($request->only('email', 'password'), $request->remember))
         {
             return back()->with('message', 'Invalid credentials');
         }
