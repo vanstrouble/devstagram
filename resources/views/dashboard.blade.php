228ed7355590e932b02be16290b0cee67a2675e8
vanstrouble <vanstrouble@proton.me>
2023-07-03 18:31:34 -0600

    Adjust username and user image

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.app')
 
 @section('title')
-    This is your accout
+    {{-- This is your accout --}}
 @endsection
 
 @section('content')
-    <div class="flex justify-center"> <!-- Agregado: Centrado horizontal -->
+    <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="w-8/12 lg:w-6/12 px-5 flex items-center justify-center h-full"> <!-- Modificado: Añadida clase "h-full" y centrado vertical y horizontal -->
-                <img src="{{ asset('img/usuario.svg') }}" alt="user image" class="w-1/2 lg:w-full">
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex justify-center">
+                <img src="{{ asset('img/usuario.svg') }}" alt="user image" class="mx-auto w-1/2 lg:w-full py-2">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5 md:flex md:flex-col md:justify-center md:items-start">
-                <p class="text-gray-700 text-2xl text-center mt-5">
-                    {{ auth()->check() ? $user->username : 'No user authenticated' }}
-                </p>
+            <div class="md:w-8/12 lg:w-6/12 px-5 md:flex md:flex-col md:justify-center">
+                <p class="text-gray-700 text-2xl text-center md:text-left">{{ $user->username }}</p>
                 <div class="flex justify-center">
                     <p class="text-gray-800 text-sm mb-3 font-bold mr-6">
                         0
