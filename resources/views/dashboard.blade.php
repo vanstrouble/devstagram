49f53e54e0cfa496bbe1570dc8f7ec883041d4f7
vanstrouble <vanstrouble@proton.me>
2023-07-03 18:11:13 -0600

    Delete if else

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,18 +5,29 @@
 @endsection
 
 @section('content')
-    <div class=" flex justify-center">
-        <div class=" w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class=" md:w-8/12 lg:w-6/12 px-5">
-                <p>Ur horrible face here</p>
-                <img src="{{ asset('img/usuario.svg') }}" alt="user image">
+    <div class="flex justify-center"> <!-- Agregado: Centrado horizontal -->
+        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
+            <div class="w-8/12 lg:w-6/12 px-5 flex items-center justify-center h-full"> <!-- Modificado: Añadida clase "h-full" y centrado vertical y horizontal -->
+                <img src="{{ asset('img/usuario.svg') }}" alt="user image" class="w-1/2 lg:w-full">
             </div>
-            <div class=" md:w-8/12 lg:w-6/12 px-5">
-                @if(auth()->check())
-                    <p class=" text-gray-700 text-2xl">{{ $user->username }}</p>
-                @else
-                    <p class=" text-gray-700 text-2xl">No user authenticated</p>
-                @endif
+            <div class="md:w-8/12 lg:w-6/12 px-5 md:flex md:flex-col md:justify-center md:items-start">
+                <p class="text-gray-700 text-2xl text-center mt-5">
+                    {{ auth()->check() ? $user->username : 'No user authenticated' }}
+                </p>
+                <div class="flex justify-center">
+                    <p class="text-gray-800 text-sm mb-3 font-bold mr-6">
+                        0
+                        <span class="font-normal">Seguidores</span>
+                    </p>
+                    <p class="text-gray-800 text-sm mb-3 font-bold mr-6">
+                        0
+                        <span class="font-normal">Siguiendo</span>
+                    </p>
+                    <p class="text-gray-800 text-sm mb-3 font-bold">
+                        0
+                        <span class="font-normal">Posts</span>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
