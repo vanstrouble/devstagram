482e00a7b67eefc21ba37d372bf7dff17208036e
vanstrouble <vanstrouble@proton.me>
2023-07-04 14:58:27 -0600

    Add dropzone rules

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,17 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable indent */
 import Dropzone from "dropzone";
 
 Dropzone.autoDiscover = false;
 
+const dropzone = new Dropzone("#dropzone", {
+    dictDefaultMessage: "Sube tu obra aquí",
+    acceptedFiles: ".png, .jpg, .jpeg, .heic",
+    addRemoveLinks: true,
+    dicRemoveFile: "Borrar Archivo",
+    maxFile: 1,
+    uploadmultiple: false,
+});
