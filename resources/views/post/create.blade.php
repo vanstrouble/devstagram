e60b462cc48faa2e0fbe8c230709059c22dabc5a
vanstrouble <vanstrouble@proton.me>
2023-07-04 14:43:34 -0600

    Change upload image to title input

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    Create a new post
+    ¡Comparte tu historia con el mundo!
 @endsection
 
 @section('content')
@@ -11,23 +11,25 @@
         </div>
 
         <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
-            <h2 class="text-2xl font-semibold mb-4">Create a new post</h2>
+            {{-- <h2 class="text-2xl font-semibold mb-4 text-center">Crea un nuevo post</h2> --}}
             <form action="#" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-4">
-                    <label for="image" class="mb-2 block uppercase text-gray-500 font-bold">Image</label>
-                    <input type="file" name="image" id="image" class="border-gray-300 border p-2 w-full"
-                        accept="image/*">
+                    <label for="title" class=" mb-2 block uppercase text-gray-500 font-bold">
+                        Título de la obra
+                    </label>
+                    <input type="text" id="title" name="title" placeholder="Introduce el título"
+                        class="border p-3 w-full rounded-lg @error('title') border-red-500 @enderror" />
                 </div>
 
                 <div class="mb-4">
-                    <label for="description" class="mb-2 block uppercase text-gray-500 font-bold">Description</label>
-                    <textarea name="description" id="description" placeholder="Description of the post"
+                    <label for="description" class="mb-2 block uppercase text-gray-500 font-bold">Descripción</label>
+                    <textarea name="description" id="description" placeholder="Descripción de la obra"
                         class="border-gray-300 border p-2 w-full" rows="4"></textarea>
                 </div>
 
                 <div class="flex justify-end">
-                    <input type="submit" value="Upload Post"
+                    <input type="submit" value="Subir"
                         class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
                 </div>
             </form>
