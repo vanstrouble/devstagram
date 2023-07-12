41282210ff9ec3fc6e456f6b4683dc161b8a7008
vanstrouble <vanstrouble@proton.me>
2023-07-12 13:33:37 -0600

    Solve some imageController problems

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -11,17 +11,21 @@ class ImageController extends Controller
     //
     public function store(Request $request)
     {
+        // Testing
         // $input = $request->all();
         // return response()->json($input);
-        $image = $request->file('file');
-        // $image = $request->file('image');
-        // $nameImage = Str::uuid() . "." . $image->extension();
-        // $imageServer = Image::make($image);
-        // $imageServer->fit(1000, 1000);
-        // $imagePath = public_path('uploads') . '/' . $nameImage;
-        // $imageServer->save($imagePath);
 
-        // return response()->json(['image' => $nameImage]);
-        return response()->json(['image' => $image->extension()]);
+        // Tranform image
+        $image = $request->file('file');
+
+        $imageName = Str::uuid() . '.' . $image->extension();
+
+        $serverImage = Image::make($image);
+        $serverImage->fit(1000, 1000);
+
+        $pathImage = public_path('uploads') . '/' . $imageName;
+        $serverImage->save($pathImage);
+
+        return response()->json(['image' => $imageName]);
     }
 }
