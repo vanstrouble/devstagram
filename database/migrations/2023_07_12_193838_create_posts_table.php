3cf354be6d7e1d74ac7914169aadbdac79c810f0
vanstrouble <vanstrouble@proton.me>
2023-07-12 13:49:14 -0600

    Create post table

--- database/migrations/2023_07_12_193838_create_posts_table.php
+++ database/migrations/2023_07_12_193838_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
