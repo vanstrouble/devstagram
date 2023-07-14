4e2f3e4ce0b242ccec2661c512b3528f086dba0c
vanstrouble <vanstrouble@proton.me>
2023-07-14 17:41:27 -0600

    Filter user posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,8 +15,12 @@ class PostController extends Controller
     //
     public function index(User $user)
     {
+        $posts = Post::where('user_id', $user->id)->get();
+        dd($posts);
+
         return view('dashboard', [
             'user' => $user,
+            'posts' => $posts
         ]);
     }
 
