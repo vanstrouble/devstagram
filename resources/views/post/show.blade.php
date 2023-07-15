49660efadc78a18f08f974a03d5d89b9f60c73d3
vanstrouble <vanstrouble@proton.me>
2023-07-15 14:49:34 -0600

    Add comment box view

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -18,8 +18,23 @@
                 <p class=" mt-5">{{ $post->description }}</p>
             </div>
         </div>
-        <div class=" md:w-1/2">
+
+        <div class="md:w-1/2 p-5">
             {{-- Comments --}}
+            <div class="comment-box shadow bg-white p-5 mb-5">
+                <p class="text-xl font-bold text-center mb-4 container mx-auto md:flex">¿Qué te hace sentir esta obra?</p>
+
+                <form action="">
+                    <div class="mb-4">
+                        <textarea name="comment" id="description" placeholder="Agrega un nuevo comentario"
+                            class="border p-2 w-full bg-gray-100 rounded-lg" rows="4"></textarea>
+                    </div>
+                    <div class="flex justify-end">
+                        <button type="submit"
+                            class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">Comentar</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
