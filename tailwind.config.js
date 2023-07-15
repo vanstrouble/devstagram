368489b0720550f90626acffd1d08e1127fce9dd
vanstrouble <vanstrouble@proton.me>
2023-07-14 19:26:21 -0600

    Add tailwind to pagination

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+/* eslint-disable quotes */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
