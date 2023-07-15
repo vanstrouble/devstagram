d1adee2caece6df71fa5bc0da4280441736e2c6d
vanstrouble <vanstrouble@proton.me>
2023-07-15 16:02:48 -0600

    Add warning to comment area

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -27,8 +27,25 @@
                     <form action="{{ route('comments.store', ['post' => $post, 'user' => $user]) }}" method="POST">
                         @csrf
                         <div class="mb-4">
-                            <textarea name="comment" id="description" placeholder="Agrega un nuevo comentario"
-                                class="border p-2 w-full bg-gray-100 rounded-lg" rows="4"></textarea>
+                            <textarea name="comment" id="comment" placeholder="Agrega un nuevo comentario"
+                                class="border p-2 w-full bg-gray-100 rounded-lg @error('comment') border-red-500 @enderror" rows="4"></textarea>
+                            @error('comment')
+                                <div class="bg-red-500 text-white my-2 rounded-lg p-4">
+                                    <div class="flex items-center">
+                                        <div class="mr-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                                class="h-4 w-4">
+                                                <path fill-rule="evenodd"
+                                                    d="M10 1C4.486 1 0 5.486 0 10c0 4.515 4.486 9 10 9 5.515 0 10-4.485 10-9 0-4.514-4.485-9-10-9zm0 16c-3.866 0-7-3.134-7-7 0-3.866 3.134-7 7-7 3.866 0 7 3.134 7 7 0 3.866-3.134 7-7 7zm0-12a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm0 4a1 1 0 0 0-1 1v4a1 1 0 0 0 2 0v-4a1 1 0 0 0-1-1z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <p class="text-sm">{{ $message }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            @enderror
                         </div>
                         <div class="flex justify-end">
                             <button type="submit"
