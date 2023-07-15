620d5d6ea59356de15e6b85bdd78e46b7767bc6d
vanstrouble <vanstrouble@proton.me>
2023-07-15 15:38:34 -0600

    Create migration

--- database/migrations/2023_07_15_212055_create_comments_table.php
+++ database/migrations/2023_07_15_212055_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->string('comment');
             $table->timestamps();
         });
     }
