396f42047be8f0d26568b895279c3deec04a9095
vanstrouble <vanstrouble@proton.me>
2023-07-19 17:15:57 -0600

    Make some changes to style on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,30 @@
 @extends('layouts.app')
 
 @section('title')
-    {{-- This is your accout --}}
+    {{-- This is your account --}}
 @endsection
 
 @section('content')
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class=" md:w-10/12 lg:w-4/12 px-5 md:p-1 flex justify-center">
+            <div class="md:w-4/12 px-5 md:p-1 flex justify-center">
                 <img src="{{ asset('img/usuario.svg') }}" alt="user image" class="mx-auto w-1/2 lg:w-full py-2">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5 md:flex md:flex-col md:justify-center">
-                <p class="text-gray-700 text-2xl text-center md:text-left">{{ $user->username }}</p>
+            <div class="md:w-8/12 px-5 md:flex md:flex-col md:justify-center">
+                <div class="flex items-center justify-center md:justify-start md:gap-2">
+                    <p class="text-gray-700 text-2xl text-center md:text-left">{{ $user->username }}</p>
+                    @auth()
+                        @if ($user->id === auth()->user()->id)
+                            <a href="" class="text-gray-500 hover:text-gray-600 cursor-pointer p-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                </svg>
+                            </a>
+                        @endif
+                    @endauth
+                </div>
                 <div class="flex justify-center md:justify-start">
                     <p class="text-gray-800 text-sm mb-3 font-bold mr-6">
                         0
@@ -30,12 +43,12 @@
         </div>
     </div>
 
-    <section class=" container mx-auto mt-10">
-        <h2 class=" text-4xl text-center font-black my-10">
+    <section class="container mx-auto mt-10">
+        <h2 class="text-4xl text-center font-black my-10">
             Shitposting
         </h2>
         @if ($posts->count() > 0)
-            <div class=" grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach ($posts as $post)
                     <div>
                         <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
