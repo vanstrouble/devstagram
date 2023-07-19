7bfd2e203e6c00b8fd855054650f72d227ab8dbf
vanstrouble <vanstrouble@proton.me>
2023-07-19 17:42:46 -0600

    Protect profile edition

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        // dd('This is the profile');
+
+    }
 }
