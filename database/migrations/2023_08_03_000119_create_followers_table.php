307dc6accc7d10b977a571aaa47efe7bf4098b91
vanstrouble <vanstrouble@proton.me>
2023-08-02 18:14:55 -0600

    Make columns to followers table

--- database/migrations/2023_08_03_000119_create_followers_table.php
+++ database/migrations/2023_08_03_000119_create_followers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
