82d4adb71313f6d776374d962b1a33c9479f5641
vanstrouble <vanstrouble@proton.me>
2023-08-13 19:48:06 -0600

    Get user following posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +10,13 @@ class HomeController extends Controller
     public function __invoke()
     {
         // dd('Testing home view');
-        return view('home');
+
+        // Get all following
+        $ids = auth()->user()->followings->pluck('id')->toArray();
+        $posts = Post::whereIn('user_id', $ids)->paginate(20);
+
+        return view('home', [
+            'posts' => $posts
+        ]);
     }
 }
