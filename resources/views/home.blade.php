b17b5313791f7a90fa767e3454c334902691dd05
vanstrouble <vanstrouble@proton.me>
2023-08-13 20:01:58 -0600

    Show following user post in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,20 @@
 @endsection
 
 @section('content')
-    Website content test
+    @if ($posts->count())
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+            @foreach ($posts as $post)
+                <div>
+                    <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
+                        <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Post Image {{ $post->title }}">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        <div class="flex items-center justify-center mt-5">
+            {{ $posts->links() }}
+        </div>
+    @else
+        <p class=" text-center">There's nothing to show</p>
+    @endif
 @endsection
