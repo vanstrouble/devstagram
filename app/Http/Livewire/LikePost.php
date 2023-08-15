2282a1df9da6adbdbef5060c43299dd6143d54f5
vanstrouble <vanstrouble@proton.me>
2023-08-14 18:38:44 -0600

    Change to reactive interfaces with livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -8,15 +8,23 @@ class LikePost extends Component
 {
 
     public $post;
+    public $isLiked;
+
+    public function mount($post)
+    {
+        $this->isLiked = $post->checkLike(auth()->user());
+    }
 
     public function like()
     {
         if ($this->post->checkLike(auth()->user())) {
             $this->post->likes()->where('post_id', $this->post->id)->delete();
+            $this->isLiked = false;
         } else {
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
+            $this->isLiked = true;
         }
     }
 
